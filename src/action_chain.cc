e409b44c24cb8750fd35c149f9d47b19c8c0d5ca
romkatv <roman.perepelitsa@gmail.com>
2018-08-31 12:29:37 +0200

    minor speed up when there is contention

--- src/action_chain.cc
+++ src/action_chain.cc
@@ -6,12 +6,15 @@ thread_local ActionChain::Mem ActionChain::mem_;
 
 void ActionChain::Work::RunAllSlow(Work* w, Work* next) {
   do {
-    assert(w != nullptr && w != Sealed());
-    assert(next != nullptr && next != Sealed());
-    w->Destroy();
-    ::operator delete(w, 64);
-    w = next;
-    w->invoke_(w);
+    do {
+      assert(w != nullptr && w != Sealed());
+      assert(next != nullptr && next != Sealed());
+      w->Destroy();
+      ::operator delete(w, 64);
+      w = next;
+      w->invoke_(w);
+      next = w->next_.load(std::memory_order_acquire);
+    } while (next);
     next = w->next_.exchange(Sealed(), std::memory_order_acq_rel);
   } while (next);
 }
